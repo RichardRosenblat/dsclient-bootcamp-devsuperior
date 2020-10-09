7e175cf7d6ea49ea393177c2fb5ebd421f0c5573
Richard <rosenblatr@gmail.com>
2020-10-08 22:22:55 -0300

    Pagination

--- backend/src/main/java/com/richarddevsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/richarddevsuperior/dsclient/entities/Client.java
@@ -3,6 +3,7 @@ package com.richarddevsuperior.dsclient.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,8 @@ public class Client implements Serializable {
 	private String cpf;
 	
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 	
